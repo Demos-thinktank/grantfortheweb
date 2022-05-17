1af1f4c821190f474beebc9d18a065cdf18216b1
Akshaya <akshayasatheesh@hotmail.co.uk>
2022-05-17 14:27:01 +0100

    Made a simple responsive landing page: grid on larger screen sizes and block elements on smaller screens

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -6,28 +6,30 @@ const LandingPage = () => (
         <BubbleWrapper className="bubble center">
             The Costs of Creation: What is a fair and desirable future for web monetisation?
         </BubbleWrapper>
-        <BubbleWrapper className="bubble"/>
+
+        <BubbleWrapper className="bubble one">
+            <Link to='one'>
+                What is web monetization?
+            </Link>        
+        </BubbleWrapper>
+
         <BubbleWrapper className="bubble two">
             <Link to='two'>
                 Why does the future of web monetisation matter?
             </Link>
         </BubbleWrapper>
+
         <BubbleWrapper className="bubble three">
             <Link to='three'>
                 What do online workers and volunteers think?
             </Link>
         </BubbleWrapper>
+        
         <BubbleWrapper className="bubble four">
             <Link to='four'>
                 What should be done?
             </Link>
         </BubbleWrapper>
-        <BubbleWrapper className="bubble one">
-            <Link to='one'>
-                What is web monetization?
-            </Link>
-        </BubbleWrapper>
-        <BubbleWrapper className="bubble"/>
     </Container>
 )
 
@@ -35,14 +37,25 @@ const Container = styled.div`
     --w:400px;
     --n:5;
     display:grid;
-    grid-template-columns:repeat(auto-fit,minmax(clamp(100%/(var(--n) + 1) + 0.1%,(var(--w) - 100vw)*1000,100%),1fr)); /*this */
-    grid-template-rows:repeat(auto-fit,minmax(clamp(100%/(var(--n) + 1) + 0.1%,(var(--w) - 100vw)*1000,100%),1fr)); /*this */
-    grid-column-gap: 0;
     grid-row-gap: 2rem;
+    width: 100vw;
+    height: 100vh;
+    @media screen and (max-width: 600px){
+        display: block;
+        background-color: aliceblue;
+        height: 100%
+    }
+
 `
 const BubbleWrapper = styled.div`
-    height: 20vh;
-    background-color: grey;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 2rem;
+    @media screen and (max-width: 600px){
+        margin: 2rem 0 0 0;
+    }
 `
 
 export default LandingPage;
