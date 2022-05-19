49575ea99026968ee94655683149a4484fd073f9
Akshaya <akshayasatheesh@hotmail.co.uk>
2022-05-19 15:07:56 +0100

    Fixed imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import './pages.css'
+import './css/pages.css'
 
 import LandingPage from './pages/LandingPage';
 import ContentOne from './pages/ContentOne';
