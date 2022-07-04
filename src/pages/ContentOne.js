ee6f75095968128f5390a160a661cdbb075a21bb
CiaranCummins <105939147+CiaranCummins@users.noreply.github.com>
2022-07-04 15:28:47 +0100

    Updating body of text

--- src/pages/ContentOne.js
+++ src/pages/ContentOne.js
@@ -5,7 +5,8 @@ const ContentOne = () => (
         <ContentLayout 
             heading="The Costs of Creation: What is a fair and desirable future for web monetisation?" 
             title="What is web monetisation?"
-            // text="change text here once styling is finished"
+            // text="Simply put, web monetisation refers to the process of turning web traffic (i.e. site visits) into a revenue stream. Online, we are surrounded by various models of web monetisation, from advertising, to subscription sign-ups, to the selling of the data collected about site visitors, to the sale of products and services, and much more.
+"
         />
     </>
 )
