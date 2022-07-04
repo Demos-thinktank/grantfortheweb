370831735d92d3542f209c97ebba209bb2bc1713
CiaranCummins <105939147+CiaranCummins@users.noreply.github.com>
2022-07-04 17:13:03 +0100

    Full text

--- src/pages/ContentTwo.js
+++ src/pages/ContentTwo.js
@@ -14,17 +14,13 @@ const ContentTwo = () => (
             title="Why does the future of web monetisation matter?"
             text=
             {`
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Phasellus varius enim et sapien placerat tincidunt. Donec a arcu 
-                ut urna volutpat scelerisque. Curabitur tempor congue laoreet. 
-                Quisque vel pretium ante, nec congue elit. Morbi condimentum 
-                lobortis arcu, vel blandit ipsum scelerisque vitae. Aliquam 
-                condimentum, orci et fermentum semper, massa ante gravida enim, 
-                vitae aliquet diam mauris sit amet eros. Vestibulum et purus 
-                imperdiet, fermentum leo et, viverra turpis. Fusce semper semper 
-                convallis. Etiam fermentum vitae velit non molestie. Aliquam 
-                elementum pretium justo ac tempor. Maecenas efficitur id ipsum 
-                id molestie. Nulla volutpat vulputate nulla, elementum rutrum mi.
+             More and more, the financial cost of our digital lives is creeping into view. There is growing awareness that each casual ‘agree’ we give to a pop-upabout cookies represents, in essence, the paying of a fee. We know that the ads we block are making it tougher to keep some sites afloat. And as the wider social costs of our current digital economy become clear - from the exploitation of workers (online and off), to the designing of platforms to swallow up users’ attention - the sustainability of the prevailing online business models is increasingly under scrutiny. All this makes the question of how the online world ought to be financed ever more pressing.
+
+Answering this means wrestling with further complexities in turn: what would it mean to pay people fairly for their goods, services, creations or labours online? Are the current forms of funding, from advertising and gig work, to subscriptions and donations, to e-commerce and product placements, the best we can do, or are there better alternatives? And as the drive to “grind” and hustle in the digital world is ever more present, what shouldn’t be paid for or considered work online anyway?
+
+Such technologies could offer a route to a digital economy in which people exercise more control over web monetisation, reducing the need to rely on external funding sources (such as advertisers) or direct user funding that is more disruptive (such as paywalls). However, it is possible this would encourage monetisation of things that ought to remain as they are, and could be co-opted by Big Tech companies largely to their gain. Whether or not this is true and whether it is this technology, another, or a set of monetisation tools that will become dominant, remains to be seen.
+
+What is certain is that from micropayments to adverts, donations to subscriptions, all forms of web monetisation shape our online experiences, from introducing or reducing friction to our browsing, to deciding what it is we see. Likewise, all of these shape how people work online and what activities they can monetise in the first place. How we pay for our online world can have, therefore, tremendous ramifications.
             `}
         />
 
