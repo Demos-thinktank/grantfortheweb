fa11120182598d228b721528ef5b379feb5fc6b7
CiaranCummins <105939147+CiaranCummins@users.noreply.github.com>
2022-07-04 15:58:07 +0100

    Update ContentOne.js

--- src/pages/ContentOne.js
+++ src/pages/ContentOne.js
@@ -5,7 +5,7 @@ const ContentOne = () => (
         <ContentLayout 
             heading="The Costs of Creation: What is a fair and desirable future for web monetisation?" 
             title="What is web monetisation?"
-            // text="Simply put, web monetisation refers to the process of turning web traffic (i.e. site visits) into a revenue stream. Online, we are surrounded by various models of web monetisation, from advertising, to subscription sign-ups, to the selling of the data collected about site visitors, to the sale of products and services, and much more."
+            // text="Simply put, web monetisation refers to the process of turning web traffic (i.e. site visits) into a revenue stream. Online, we are surrounded by various models of web monetisation, from advertising, to subscription sign-ups, to the selling of the data collected about site visitors, to the sale of products and services, and much more. The financing of things online can of course sit outside the realm of private enterprise too. For example, when you visit the Gov.uk site, you’re doing so thanks to public funds. Furthermore, a site’s financial support might come from donations given by visitors or philanthropic bodies, as is the case with Wikipedia. You also have the world of non-proprietary, open-source entities online which are typically run at their core by volunteers, like the social media site Mastodon. There is also a growing movement of cooperatively owned platforms, like the music streaming service Resonate."
         />
     </>
 )
